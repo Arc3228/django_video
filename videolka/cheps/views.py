04f9802d1efa9d62dcb32ef192a8eb9922826a79
Raf <rafael.dronov36@gmail.com>
2024-09-17 08:27:20 +0300

    INT

--- videolka/cheps/views.py
+++ videolka/cheps/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
 def home_page(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Home page")
+    return HttpResponse("The Office - смотреть")
